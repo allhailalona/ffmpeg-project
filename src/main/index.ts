97911dbe7dbcb837edc82f4d57548243ecd15894
Lotan Bar <lotanbar3@gmail.com>
2024-07-23 13:37:11 -0700

    filesdropzone/1 	open dev pane by default when on dev mode 	implement dnd and manipulate output of e.dataTransfer.files 	implement functionality to dispatch reducer to update explorer

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,8 +7,8 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 400,
-    height: 300,
+    width: 800,
+    height: 350,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -16,7 +16,13 @@ function createWindow(): void {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
     }
+
   })
+
+  //open dev pane by default when on dev mode
+  if (is.dev) {
+    mainWindow.webContents.openDevTools()
+  }
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
@@ -51,24 +57,28 @@ app.whenReady().then(() => {
   })
 
   //add your ipcHandlers here
-  ipcMain.handle('GET_DETAILS', async (event, explorer: DirItem[]) => {
+  ipcMain.handle('alona', (event, value) => {
+    console.log(value)
+  })
+
+  ipcMain.handle('GET_DETAILS', async (event, dirsToDetail: DirItem[] | []) => {
+    console.log('welcome! dirsToDetail is ', dirsToDetail)
     //the problem Gal found was HERE! check on youtube how to use promise.all settler
-    return Promise.allSettled(explorer.map(dir => {
+    return Promise.allSettled(dirsToDetail.map(dir => {
       return getItemDetails(dir)
     }))
     .then(results => {
-      const updatedExplorer = results.map((result, index) => {
+      const updatedDirsToDetail = results.map((result, index) => {
         if (result.status === 'fulfilled') {
           //if the value is okay, insert it to the array in the proper location
-          return {...explorer[index], ...result.value}
+          return {...dirsToDetail[index], ...result.value}
         } else {
           //if it's not, don't insert it (avoid nulls this)
           console.error(`Error processing dir ${index}:`, result.reason);
-          return explorer[index]; // Return original item if there was an error
+          return dirsToDetail[index]; // Return original item if there was an error
         }
       })
-      console.log(updatedExplorer)
-      return updatedExplorer
+      return updatedDirsToDetail
     })
     .catch(err => {
       console.error('Error in GET_DETAILS handler:', err);
@@ -76,21 +86,22 @@ app.whenReady().then(() => {
     })
   })
 
-  ipcMain.handle('TOGGLE_EXPAND', async (event, dirToToggle: DirItem): Promise<DirItem> => {
+  ipcMain.handle('TOGGLE_EXPAND', async (event, dirToToggle: DirItem): Promise<DirItem[]> => {
     //dirToToggle must be passed as a whole to index.ts since we need to check the value of isExpanded
+    console.log(dirToToggle)
     try {
-      if(dirToToggle.isExpanded) {
+      if(!dirToToggle.isExpanded) {
         //find subfolders
         const subfolders = await fs.promises.readdir(dirToToggle.path)
         
         //create a path value for each dir
         const expandedDirToToggle = subfolders.map(dir => ({path: path.join(dirToToggle.path, dir)}))
+        console.log('fuck', expandedDirToToggle)
 
         //detail each path item, since we're inside try{} and not .then() we need to use await
-        await Promise.allSettled(expandedDirToToggle.map(dir => {
+        const detailedExpandedDirToToggle = await Promise.allSettled(expandedDirToToggle.map(dir => {
           return getItemDetails(dir)
-        })).then(results => {
-          const detailedExpandedDirToToggle = results.map((result, index) => {
+        })).then(results => results.map((result, index) => {
             if (result.status === 'fulfilled') {
               return {...expandedDirToToggle[index], ...result.value}
             } else {
@@ -98,22 +109,22 @@ app.whenReady().then(() => {
               return expandedDirToToggle[index]
             }
           })
-          console.log('detailed expanded list: ', detailedExpandedDirToToggle)
-          return detailedExpandedDirToToggle
-        }).catch(err => {
+        ).catch(err => {
           console.error('Error in TOGGLE_EXPAND detailing handler:', err);
           throw err;
         })
+
+        return detailedExpandedDirToToggle
       } else {
         console.log('dirToToggle is not expanded')
       }
     } catch (err) {
       console.error(err)
+      //don't forget to delete subfolders here!!!
     }
   })
 
   async function getItemDetails(dir: DirItem) {
-    console.log('hello from getItemDetails, detail recieved is')
     try {
       const stats = await fs.promises.stat(dir.path);
     
@@ -139,7 +150,7 @@ app.whenReady().then(() => {
         }
       }
     } catch (err) {
-      console.error(`Couldn't detail ${dir.path}:`, err);
+      console.error(`Couldn't detail ${dir.path}`);
       return null //null will appear anyways, I'm gonna filter it in the expand functino above
     }
 
--- src/renderer/src/comps/FileView.tsx
+++ src/renderer/src/comps/FileView.tsx
@@ -17,63 +17,66 @@ interface Action {
 	payload?: any
 }
 
+function updateExplorerRecursively(dirs: DirItem[], parentDir: DirItem, subfolders?: DirItem[]): DirItem[] {
+	return dirs.map(dir => {
+		if(dir.path === parentDir.path) {
+			const updatedDir = {
+				...dir, 
+				subfolders: subfolders
+			}
+			console.log('found and added ', updatedDir)
+			return updatedDir
+		} else if (dir.subfolders && dir.isExpanded) {
+			console.log('couldnt find in parent folders, searching in sub directories')
+			return {
+				...dir, 
+				subfolders: updateExplorerRecursively(dir.subfolders, parentDir, subfolders)
+			}
+		}
+		//if nothing was found
+		return dir
+	})
+}
+
 //the state is managed automatically and there is no need to explicitly pass it to the function
 function reducer(explorer: DirItem[], action: Action) {
 	switch (action.type) {
 		case 'GET_DETAILS':
-			console.log(action.payload)
-			return action.payload
+			console.log(action.payload.res)
+			return action.payload.res
 		case 'TOGGLE_EXPAND':
 			if(action.payload.dirToToggle !== undefined) {
+				//toggle dir upon click
 				action.payload.dirToToggle.isExpanded = !action.payload.dirToToggle.isExpanded
-			} else if (action.payload.toggledDir !== undefined) {
-				console.log('allhailhitler')
-				console.log(action.payload.toggledDir)
+			} else if (action.payload.parentDir !== undefined && action.payload.subfolders !== undefined) {
+				//update explorer
+				const result = updateExplorerRecursively(explorer, action.payload.parentDir, action.payload.subfolders)
+				return result
 			}
 		default:
 			return explorer
 	}
 }
 
-const initDir: DirItem[] = [
-	{path: "C:/Users/user/Desktop"},
-	{path: "C:/Users/user/Documents"}
-]
-
 export default function FileView(): JSX.Element {
 	//right now we use initDir for sake of example
-	const [explorer, dispatch] = useReducer(reducer, initDir)
+	const [explorer, dispatch] = useReducer(reducer, [])
 	const [isLoading, setIsLoading] = useState(true)
-
-	//GET_DETAILS once the app runs, int he future this won't be the data I specified but the dropped paths
-	useEffect(() => {
-		const fetchDetails = async (): Promise<void> => {
-			try {
-				setIsLoading(true)
-				const updatedExplorer = await window.electron.ipcRenderer.invoke('GET_DETAILS', explorer)
-				dispatch({type: 'GET_DETAILS', payload: updatedExplorer})
-			} catch (err) {
-				console.error(err)
-			} finally {
-				setIsLoading(false)
-			}
-		}
-
-		fetchDetails()
-	}, [])
 
 	const toggleExpand = async (dirToToggle: DirItem) => {
 		try {
 			setIsLoading(true)
+
 			//toggle clickedDir
-			dispatch({type: 'TOGGLE_EXPAND', payload: {dirToToggle: dirToToggle}})
+			dispatch({type: 'TOGGLE_EXPAND', payload: {dirToToggle}})
 
 			//expand/collapse dir
-			const toggledDir = await window.electron.ipcRenderer.invoke('TOGGLE_EXPAND', dirToToggle)
+			//in the future u can pass the dir alone, which I presume will save CPU... 
+			const subfolders = await window.electron.ipcRenderer.invoke('TOGGLE_EXPAND', dirToToggle)
+			
+			//update explorer
+			dispatch({type: 'TOGGLE_EXPAND', payload: {parentDir: dirToToggle, subfolders}})
 
-			//update explorer 
-			console.log('calling update dispatch')
-			dispatch({type: 'TOGGLE_EXPAND', payload: {toggledDir: toggledDir}})
 		} catch (err) {
 			console.error(err)
 		} finally {
@@ -82,13 +85,14 @@ export default function FileView(): JSX.Element {
 	}
 
 	const renderDirTree = (dirToRender: DirItem[]) => {
-		console.log(dirToRender)
 		return dirToRender.map(dir => {
 			if (dir.type === 'folder') {
 				return <div key={dir.path}>
-					<a onClick={() => toggleExpand(dir)}>{dir.isExpanded ? '>' : '<'}</a>
+					<a onClick={() => toggleExpand(dir)}>{dir.isExpanded ? '^  ' : 'v  '}</a>
 					{dir.metadata?.name || dir.path}
-					{dir.isExpanded && dir.subfolders && (<div style={{marginLeft: '5px'}}>{renderDirTree(dir.subfolders)}</div>)}
+					{dir.isExpanded && dir.subfolders && (
+							<div style={{marginLeft: '10px'}}>{renderDirTree(dir.subfolders)}</div>
+            )}
 				</div>
 			} else if (dir.type === 'file') {
 				return <div key={dir.path}>
@@ -100,10 +104,31 @@ export default function FileView(): JSX.Element {
 		})
 	}
 
+	const handleDragHover = (e: React.DragEvent<HTMLDivElement>) => {
+		e.preventDefault()
+	}
+
+	const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+		e.preventDefault()
+		try {
+			//since the output of e.dataTransfer.files is rather odd, we need to arrange it, so GET_DETAILS can read it
+			const droppedFiles = Array.from(e.dataTransfer.files).map(file => ({path: file.path}))
+			console.log(droppedFiles)
+		
+			const res = await window.electron.ipcRenderer.invoke('GET_DETAILS', droppedFiles)
+			console.log('res is', res)
+			dispatch({type: 'GET_DETAILS', payload: {res}})
+		} catch (err) {
+			console.error(err)
+		} finally {
+			setIsLoading(false)
+		}
+	}
 
 	//make sure page contents are not loaded until async ops are done!
-	//this is VERY important and can cause a variety of frustrating bugs
-	if (isLoading) {
+	if (explorer.length === 0) {
+		return <div onDragOver={handleDragHover} onDrop={handleDrop} style={{height:'200px', backgroundColor: 'lightblue'}}>drop files here</div>
+	} else if (isLoading) {
     return <div>Loading...</div>
   } else {
 		return (
